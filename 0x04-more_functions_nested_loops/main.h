0e5fb89ef1fd7339a3371f7aa69ebe42d66a16c2
beidemmtm <beidemmtm@gmail.com>
2022-12-16 18:21:03 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File - main.h
+ * Author: beide
+ * Desc: Header contains protypes for allfunctions
+ * used in 0x04-More_functions_nested_loops directory
+ */
+
+int _putchar(char c);
+
+#endif
