e7491fe51cd848d1a53fe29f4757a9cc4dc865e0
beidemmtm <beidemmtm@gmail.com>
2022-12-20 18:38:55 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
  * _puts - prints a string, followed by a new line, to stdout
+ *
  * @str: string to print
  */
-void _pust(char *str)
+
+void _puts(char *str)
 {
 	while (*str != '\0')
 	{
